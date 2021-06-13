40f6cc80e71eade1c299e66411eb9fad7f336133
Ashish K <dev.ashishkumar@gmail.com>
2021-06-13 15:17:54 +0530

    add video details in form hidden inputs and changes in index.js file

--- routes/index.js
+++ routes/index.js
@@ -15,18 +15,42 @@ router.post('/', function (req, res, next) {
   // ytdl(req.body.url, {
   //   format: 'mp4'
   //   }).pipe(res);
+  if (ytdl.validateURL(req.body.url)) {
+    ytdl.getBasicInfo(req.body.url).then(res => res).then(
+      json => {
+        // console.log(json)
+        res.render('index', {
+          title: 'Download You Tube Video',
+          videoData: json,
+          url: req.body.url
+        });
+      }
+    );
+  } else {
+    res.render('index', { title: 'Download You Tube Video' , videoData: '', url: ''});
+  }
 
-  ytdl.getBasicInfo(req.body.url).then(res => res).then(
-    json => {
-      // console.log(json)
-      res.render('index', {
-        title: 'Download You Tube Video',
-        videoData: json,
-        url: req.body.url
-      });
+});
+
+router.post('/download', function (req, res, next) {
+  // res.header('Content-Disposition', 'attachment; filename="video.mp4"');
+  // ytdl(req.body.url, {
+  //   format: 'mp4'
+  //   }).pipe(res);
+  res.header('Content-Disposition', 'attachment; filename="video.mp4"');
+  const info = {
+    vid: '',
+    uid: ''
+  };
+  const options = {
+    quality: 'highest',
+    filter: 'audioandvideo',
+    format: {
+      itag: '',
+      url: ''
     }
-  );
-
+  };
+  ytdl.downloadFromInfo(info, options).pipe(res);
 
 });
 
