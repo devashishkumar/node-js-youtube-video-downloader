63b32f2fe9166e431e83668f18d41845834f761b
Ashish K <dev.ashishkumar@gmail.com>
2021-06-19 11:54:18 +0530

    add download feature along with css changes

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,4 @@
+const { resolveNaptr } = require('dns');
 var express = require('express');
 const fs = require('fs')
 const youtubedl = require('youtube-dl-exec');
@@ -33,24 +34,48 @@ router.post('/', function (req, res, next) {
 });
 
 router.post('/download', function (req, res, next) {
+  req.body.format = JSON.parse(req.body.format);
+  let mimeType = '';
+  const mimeTypes = ['mp4', 'webm'];
+  mimeTypes.forEach(mt => {
+    if (req.body.format.mimeType.includes(mt)) {
+      mimeType = mt;
+    }
+  });
+  const fileName = `${req.body.title}-${req.body.format.qualityLabel}.${mimeType}`;
+
+  // res.header('Content-Disposition', `attachment; filename=${fileName}`);
+
+  res.header('Content-Disposition', `attachment; filename=${fileName}`);
+  ytdl(req.body.url, { filter: 'audioandvideo',
+  qualityLabel: req.body.format.qualityLabel,
+  width: req.body.format.width,
+  height: req.body.format.height,
+  bitrate: req.body.format.bitrate,
+  itag: req.body.format.itag,
+  // quality: req.body.format.quality,
+  container: mimeType}).pipe(res);
+
+});
+
+router.post('/downloadvideo', function(req, res, next) {
   // res.header('Content-Disposition', 'attachment; filename="video.mp4"');
   // ytdl(req.body.url, {
   //   format: 'mp4'
   //   }).pipe(res);
-  res.header('Content-Disposition', 'attachment; filename="video.mp4"');
-  const info = {
-    vid: '',
-    uid: ''
-  };
-  const options = {
-    quality: 'highest',
-    filter: 'audioandvideo',
-    format: {
-      itag: '',
-      url: ''
-    }
-  };
-  ytdl.downloadFromInfo(info, options).pipe(res);
+  // res.header('Content-Disposition', 'attachment; filename="video.mp4"');
+  // const info = {
+  //   vid: '',
+  //   uid: ''
+  // };
+  // const options = {
+  //   quality: 'highest',
+  //   filter: 'audioandvideo',
+  //   format: {
+  //     itag: req.body.itag,
+  //     url: ''
+  //   }
+  // };
 
 });
 
