dc5913029246ed86bfac10b705366e16c85b4110
Ashish K <dev.ashishkumar@gmail.com>
2021-06-19 13:09:15 +0530

    use async await for video information and download

--- routes/index.js
+++ routes/index.js
@@ -11,29 +11,35 @@ router.get('/', function (req, res, next) {
   res.render('index', { title: 'Download You Tube Videos' , videoData: '', url: ''});
 });
 
-router.post('/', function (req, res, next) {
-  // res.header('Content-Disposition', 'attachment; filename="video.mp4"');
-  // ytdl(req.body.url, {
-  //   format: 'mp4'
-  //   }).pipe(res);
+router.post('/', async function (req, res, next) {
   if (ytdl.validateURL(req.body.url)) {
-    ytdl.getBasicInfo(req.body.url).then(res => res).then(
-      json => {
-        // console.log(json)
-        res.render('index', {
-          title: 'Download You Tube Video',
-          videoData: json,
-          url: req.body.url
-        });
-      }
-    );
+    try {
+      const result = await ytdl.getBasicInfo(req.body.url);
+      res.render('index', {
+        title: 'Download You Tube Video',
+        videoData: result,
+        url: req.body.url
+      });
+    } catch(e) {
+      console.error(e);
+    }
+    // ytdl.getBasicInfo(req.body.url).then(res => res).then(
+    //   json => {
+    //     // console.log(json)
+    //     res.render('index', {
+    //       title: 'Download You Tube Video',
+    //       videoData: json,
+    //       url: req.body.url
+    //     });
+    //   }
+    // );
   } else {
     res.render('index', { title: 'Download You Tube Videos' , videoData: '', url: ''});
   }
 
 });
 
-router.post('/download', function (req, res, next) {
+router.post('/download', async function (req, res, next) {
   req.body.format = JSON.parse(req.body.format);
   let mimeType = '';
   const mimeTypes = ['mp4', 'webm'];
@@ -43,18 +49,31 @@ router.post('/download', function (req, res, next) {
     }
   });
   const fileName = `${req.body.title}-${req.body.format.qualityLabel}.${mimeType}`;
+  try {
+    res.header('Content-Disposition', `attachment; filename=${fileName}`);
+    await ytdl(req.body.url, { filter: 'audioandvideo',
+    qualityLabel: req.body.format.qualityLabel,
+    width: req.body.format.width,
+    height: req.body.format.height,
+    bitrate: req.body.format.bitrate,
+    itag: req.body.format.itag,
+    // quality: req.body.format.quality,
+    container: mimeType}).pipe(res);
+  } catch(e) {
+
+  }
 
   // res.header('Content-Disposition', `attachment; filename=${fileName}`);
 
-  res.header('Content-Disposition', `attachment; filename=${fileName}`);
-  ytdl(req.body.url, { filter: 'audioandvideo',
-  qualityLabel: req.body.format.qualityLabel,
-  width: req.body.format.width,
-  height: req.body.format.height,
-  bitrate: req.body.format.bitrate,
-  itag: req.body.format.itag,
-  // quality: req.body.format.quality,
-  container: mimeType}).pipe(res);
+  // res.header('Content-Disposition', `attachment; filename=${fileName}`);
+  // ytdl(req.body.url, { filter: 'audioandvideo',
+  // qualityLabel: req.body.format.qualityLabel,
+  // width: req.body.format.width,
+  // height: req.body.format.height,
+  // bitrate: req.body.format.bitrate,
+  // itag: req.body.format.itag,
+  // // quality: req.body.format.quality,
+  // container: mimeType}).pipe(res);
 
 });
 
